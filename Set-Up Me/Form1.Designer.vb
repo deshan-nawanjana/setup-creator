dc32e395604e1dab7530686fec7bd0af38a0343a
Deshan Nawanjana <deshan.uok@gmail.com>
2021-12-05 20:26:07 +0530

    Initital Commit

---
+++ Set-Up Me/Form1.Designer.vb
@@ -0,0 +1,433 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.t1 = New System.Windows.Forms.TextBox
+        Me.t4 = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.t3 = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.t2 = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.t5 = New System.Windows.Forms.Label
+        Me.fbb = New System.Windows.Forms.FolderBrowserDialog
+        Me.t6 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.t7 = New System.Windows.Forms.Label
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Label9 = New System.Windows.Forms.Label
+        Me.op1 = New System.Windows.Forms.OpenFileDialog
+        Me.op2 = New System.Windows.Forms.OpenFileDialog
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.lb = New System.Windows.Forms.ListBox
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.t8 = New System.Windows.Forms.Label
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.fbb2 = New System.Windows.Forms.FolderBrowserDialog
+        Me.tim = New System.Windows.Forms.Timer(Me.components)
+        Me.la = New System.Windows.Forms.TextBox
+        Me.Label10 = New System.Windows.Forms.Label
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.Label11 = New System.Windows.Forms.Label
+        Me.Label12 = New System.Windows.Forms.Label
+        Me.Label13 = New System.Windows.Forms.Label
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Setup Name"
+        '
+        't1
+        '
+        Me.t1.Location = New System.Drawing.Point(173, 6)
+        Me.t1.Name = "t1"
+        Me.t1.Size = New System.Drawing.Size(397, 26)
+        Me.t1.TabIndex = 1
+        '
+        't4
+        '
+        Me.t4.Location = New System.Drawing.Point(173, 102)
+        Me.t4.Multiline = True
+        Me.t4.Name = "t4"
+        Me.t4.Size = New System.Drawing.Size(397, 89)
+        Me.t4.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(143, 18)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Welcome Description"
+        '
+        't3
+        '
+        Me.t3.Location = New System.Drawing.Point(173, 70)
+        Me.t3.Name = "t3"
+        Me.t3.Size = New System.Drawing.Size(397, 26)
+        Me.t3.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 18)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Welcome Title"
+        '
+        't2
+        '
+        Me.t2.Location = New System.Drawing.Point(173, 38)
+        Me.t2.Name = "t2"
+        Me.t2.Size = New System.Drawing.Size(397, 26)
+        Me.t2.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 41)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 18)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Window Title"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 295)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(122, 18)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Select Files Folder"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(229, 289)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(51, 30)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        't5
+        '
+        Me.t5.AutoSize = True
+        Me.t5.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.t5.Location = New System.Drawing.Point(286, 297)
+        Me.t5.Name = "t5"
+        Me.t5.Size = New System.Drawing.Size(0, 14)
+        Me.t5.TabIndex = 10
+        '
+        't6
+        '
+        Me.t6.AutoSize = True
+        Me.t6.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.t6.Location = New System.Drawing.Point(286, 334)
+        Me.t6.Name = "t6"
+        Me.t6.Size = New System.Drawing.Size(0, 14)
+        Me.t6.TabIndex = 13
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(229, 325)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(51, 30)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.Red
+        Me.Label7.Location = New System.Drawing.Point(12, 331)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(216, 18)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Main Side Image (205 x 312)px"
+        '
+        't7
+        '
+        Me.t7.AutoSize = True
+        Me.t7.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.t7.Location = New System.Drawing.Point(286, 371)
+        Me.t7.Name = "t7"
+        Me.t7.Size = New System.Drawing.Size(0, 14)
+        Me.t7.TabIndex = 16
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(229, 361)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(51, 30)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "..."
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.ForeColor = System.Drawing.Color.Blue
+        Me.Label9.Location = New System.Drawing.Point(12, 367)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(201, 18)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Headline Image (186 x 58)px"
+        '
+        'op1
+        '
+        '
+        'op2
+        '
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 400)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(131, 18)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Def. Installing Path"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 430)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(147, 36)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Open File after Install" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Select it) "
+        '
+        'lb
+        '
+        Me.lb.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lb.FormattingEnabled = True
+        Me.lb.ItemHeight = 14
+        Me.lb.Location = New System.Drawing.Point(173, 430)
+        Me.lb.Name = "lb"
+        Me.lb.Size = New System.Drawing.Size(387, 130)
+        Me.lb.TabIndex = 20
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(264, 590)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(197, 30)
+        Me.Button4.TabIndex = 21
+        Me.Button4.Text = "Hoooo ! Create !"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(264, 567)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(181, 17)
+        Me.CheckBox1.TabIndex = 22
+        Me.CheckBox1.Text = "Show our Copyright in the setup"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        't8
+        '
+        Me.t8.AutoSize = True
+        Me.t8.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.t8.Location = New System.Drawing.Point(286, 402)
+        Me.t8.Name = "t8"
+        Me.t8.Size = New System.Drawing.Size(0, 14)
+        Me.t8.TabIndex = 24
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(229, 394)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(51, 30)
+        Me.Button5.TabIndex = 23
+        Me.Button5.Text = "..."
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'tim
+        '
+        Me.tim.Interval = 300
+        '
+        'la
+        '
+        Me.la.Location = New System.Drawing.Point(172, 197)
+        Me.la.Multiline = True
+        Me.la.Name = "la"
+        Me.la.Size = New System.Drawing.Size(397, 82)
+        Me.la.TabIndex = 26
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 200)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(132, 18)
+        Me.Label10.TabIndex = 25
+        Me.Label10.Text = "License Agreement"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Set_Up_Me.My.Resources.Resources.Untitled
+        Me.PictureBox1.Location = New System.Drawing.Point(589, 6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(434, 614)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 27
+        Me.PictureBox1.TabStop = False
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.ForeColor = System.Drawing.Color.Blue
+        Me.Label11.Location = New System.Drawing.Point(753, 385)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(201, 18)
+        Me.Label11.TabIndex = 29
+        Me.Label11.Text = "Headline Image (186 x 58)px"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.ForeColor = System.Drawing.Color.Red
+        Me.Label12.Location = New System.Drawing.Point(753, 197)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(216, 18)
+        Me.Label12.TabIndex = 28
+        Me.Label12.Text = "Main Side Image (205 x 312)px"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label13.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label13.Location = New System.Drawing.Point(12, 605)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(87, 18)
+        Me.Label13.TabIndex = 30
+        Me.Label13.Text = "VapcherSoft"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(1019, 632)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.la)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.t8)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.lb)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.t7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.t6)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.t5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.t2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.t3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.t4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.t1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Set-Up Me"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents t1 As System.Windows.Forms.TextBox
+    Friend WithEvents t4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents t3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents t2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents t5 As System.Windows.Forms.Label
+    Friend WithEvents fbb As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents t6 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents t7 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents op1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents op2 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents lb As System.Windows.Forms.ListBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents t8 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents fbb2 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents tim As System.Windows.Forms.Timer
+    Friend WithEvents la As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+
+End Class
---
+++ Set-Up Me/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Set-Up Me")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Set-Up Me")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2e04ca02-c595-4869-8b0a-8d286d6ca156")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
